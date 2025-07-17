418e278183e7203b88337598b06e0a2b238dac9e
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-07-17 04:08:16 +0100

    Update components.py

--- components.py
+++ components.py
@@ -1,8 +1,13 @@
 from PIL import Image
 import streamlit as st
 
+
+
 try:
     img = Image.open("assets/logo.png")
     st.image(img, caption="If you see this, logo path is OK")
 except FileNotFoundError:
     st.error("🚫 Logo file not found at: assets/logo.png")
+
+def render_header(active="home"):
+    # your function body here
