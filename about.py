67828c4af0e35ae83645c7798b662ffc7df63ec8
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-07-19 11:44:56 +0100

    Update about.py

--- about.py
+++ about.py
@@ -75,3 +75,5 @@ def show_about():
     st.markdown("### Join Us in Transforming Healthcare", unsafe_allow_html=True)
     st.markdown("""
     We are open to collaborations, pilots and partnerships. Let’s make healthcare smarter, together.
+    """)
+
