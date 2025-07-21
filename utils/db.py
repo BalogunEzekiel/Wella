4e5ccb7d5cb340f912f5c58205d9814ed4a7fe92
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-07-21 10:52:11 +0100

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -2,3 +2,17 @@ import sqlite3
 
 def get_connection():
     return sqlite3.connect("wella.db")
+
+def init_db():
+    conn = sqlite3.connect("wella.db")
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            fullname TEXT NOT NULL
+            email TEXT PRIMARY KEY,
+            password TEXT NOT NULL,
+            role TEXT NOT NULL
+        )
+    """)
+    conn.commit()
+    conn.close()
