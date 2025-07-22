b74d739178dd40577521c11f99da9827920dbaa1
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-07-22 21:11:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from PIL import Image
 from utils.db import init_db
+from pytz import timezone
 
 # Import pages
 from pages import landing
